a28eada27d446cc6c633ea73ec61b59ec5580cfb
omichsam <omichsam@gmail.com>
2026-03-30 13:40:24 +0300

    update added the notifacication redirection on small devices

--- connect.php
+++ connect.php
@@ -1,8 +1,9 @@
 <?php
 $host="localhost";
 $username="root";
-$password="A073955@amk77";
-$db_name="live2022whiteboxmay10mega";
+// $password="A073955@amk77";
+$password="";
+// $db_name="live2022whiteboxmay10mega";
 $db_name="whitebox";
 
 $Today=date("Y-m-d");
